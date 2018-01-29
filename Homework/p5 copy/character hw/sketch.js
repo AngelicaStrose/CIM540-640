172f95750a6e2eef954e50d921f994c4f46b95c7
Angelica <angelicastrose@gmail.com>
2018-01-29 16:15:20 -0500

    HW2 Added (Correct location)

--- Homework/p5 copy/character hw/sketch.js
+++ Homework/p5 copy/character hw/sketch.js
@@ -1,7 +1,32 @@
+var centerX = 300;
+var centerY = 300;
+
+
 function setup() {
-  // put setup code here
+
+   createCanvas(400,400);
+   background(255);
+
+     // put setup code here
 }
 
 function draw() {
-  // put drawing code here
+stroke(135,200,42);
+strokeWeight(10);
+fill(135,200,42);
+rect(centerX-200, centerY-250, 200, 300, 350);
+stroke(1);
+strokeWeight(10);
+fill(1);
+triangle(70, 150, 170, 40, 86, 55);
+stroke(1);
+strokeWeight(50);
+point(68,45);
+stroke(255);
+point(150, 175);
+point(240, 175);
+strokeWeight(2);
+line(125, 250, 275, 250);
+
+
 }
